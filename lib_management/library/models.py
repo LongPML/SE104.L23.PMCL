19d8c0970b7b8232b985d202d92a0cb44a9f00e7
JTNghia <19521899@gm.uit.edu.vn>
2021-06-15 20:08:51 +0700

    connect Databse Add Book

--- lib_management/library/models.py
+++ lib_management/library/models.py
@@ -3,20 +3,16 @@ from django.db import models
 
 # Create your models here.
 class Books(models.Model):
+	id = models.IntegerField(primary_key=True)
 	title = models.CharField(max_length=30)
 	position = models.IntegerField()
 	state = 1
 
 class Authors(models.Model):
+	id = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length=30)
 
 class Subjects(models.Model):
+	id = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length=30)
 
-class Authors_Books(models.Model):
-	authorid = models.CharField(max_length=10)
-	bookid = models.CharField(max_length=10)
-
-class Subjects_Books(models.Model):
-	subjectid = models.CharField(max_length=10)
-	bookid = models.CharField(max_length=10)
--- lib_management/library/views.py
+++ lib_management/library/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from library.models import *
+import pyodbc
+conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                      'Server=ADMIN;' 
+                      'Database=QLTV2;'
+                      'Trusted_Connection=yes;')
+cursor = conn.cursor()
 # Create your views here.
 def home_view(request, *args, **kwargs):
     print(request.user)
@@ -14,7 +20,31 @@ def admin_login(request, *args, **kwargs):
     return render(request, "adminlogin.html", {})
 
 def BookAdd(request, *args, **kwargs):
-        return render(request, "BookAdd.html", {})
+    Book = Books()
+    Book.title = request.POST.get('title')
+    Book.position = request.POST.get('position')
+    Author = Authors()
+    Authors.name = request.POST.get('authorname')
+    author_names = [i[0] for i in cursor.execute(f"SELECT NAME FROM AUTHORS")]
+    Subject = Subjects()
+    Subject.name = request.POST.get('subjectname')
+    subject_names = [i[0] for i in cursor.execute(f"SELECT NAME FROM SUBJECTS")]
+    cursor.execute(f"insert into BOOKS (TITLE, STATE, POSITION) values(N'{Book.title}',{Book.state},N'{Book.position}')")
+    cursor.commit()
+    if Authors.name not in author_names:
+        cursor.execute(f"insert into AUTHORS values(N'{Authors.name}')")
+        cursor.commit()
+    if Subject.name not in subject_names:
+        cursor.execute(f"insert into SUBJECTS values(N'{Subject.name}')")
+        cursor.commit()
+    last_book_id = [i[0] for i in cursor.execute(f"SELECT BOOK_ID FROM BOOKS WHERE TITLE = N'{Book.title}' ")][-1]
+    last_author_id = [i[0] for i in cursor.execute(f"SELECT AUTHOR_ID FROM AUTHORS WHERE NAME = N'{Authors.name}' ")][-1]
+    last_subject_id = [i[0] for i in cursor.execute(f"SELECT SUBJECT_ID FROM SUBJECTS WHERE NAME = N'{Subject.name}' ")][-1]
+    cursor.execute(f"insert into AUTHORS_BOOKS values({last_author_id},{last_book_id})")
+    cursor.commit()
+    cursor.execute(f"insert into SUBJECTS_BOOKS values({last_subject_id},{last_book_id})")
+    cursor.commit()
+    return render(request, "BookAdd.html", {})
 
 def BookDelete(request, *args, **kwargs):
         return render(request, "BookDelete.html", {})
