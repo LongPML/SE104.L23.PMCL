73b0d78a3e74bb374aabf821c9a1d1773de1b21b
duchuy3007 <19521634@gm.uit.edu.vn>
2021-06-18 21:48:03 +0700

    a

--- lib_management/library/views.py
+++ lib_management/library/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+import library.models
 from library.models import *
 import pyodbc
+import datetime
+from django.http import HttpResponse
 conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                       'Server=ADMIN;' 
                       'Database=QLTV;'
@@ -30,8 +33,8 @@ def BookAdd(request, *args, **kwargs):
     Subject.name = request.POST.get('subjectname')
     subject_names = [i[0] for i in cursor.execute(f"SELECT NAME FROM SUBJECTS")]
     if Book.title != None and Book.position !=None and Authors.name != None and Subject.name != None:
-        cursor.execute(f"insert into BOOKS (TITLE, STATE, POSITION)+\
-                         values(N'{Book.title}',{Book.state},N'{Book.position}')")
+        cursor.execute(f"""insert into BOOKS (TITLE, STATE, POSITION)
+                         values(N'{Book.title}',{Book.state},N'{Book.position}')""")
         cursor.commit()
         if Authors.name not in author_names:
             cursor.execute(f"insert into AUTHORS values(N'{Authors.name}')")
@@ -59,14 +62,56 @@ def CardAdd(request, *args, **kwargs):
     card.libcardid = request.POST.get('libcardid')
     card.borrow_date = request.POST.get('borrow_date')
     card.due_date = request.POST.get('due_date')
-    card.return_date = request.POST.get('return_date')
-    if card.bookid != None and card.libcardid != None and card.borrow_date != None and card.due_date != None:
+    # card.borrow_date = datetime.datetime.strptime(request.POST.get('date'),"%Y-%m-%d").date()
+    if card.bookid != None and card.libcardid != None and card.borrow_date != "" and card.due_date != "":
         cursor.execute(f"""insert into BORROWCARDS (BOOK_ID, LIBCARD_ID, BORROW_DATE, DUE_DATE, RETURN_DATE)
-                         VALUES({card.bookid},{card.libcardid},'{card.borrow_date}','{card.due_date}','{card.return_date}')""")
+                         VALUES({card.bookid},{card.libcardid},'{card.borrow_date}','{card.due_date}',{card.return_date})""")
+        cursor.commit()
+        cursor.execute(f"""update BOOKS set STATE = 0 WHERE BOOK_ID = {card.bookid}""")
         cursor.commit()
         return render(request, "CardAdd.html", {})
     else:
         return render(request, "CardAdd.html", {})
+
+def CardDetail(request, *args, **kwargs):
+
+    result = cursor.execute(f"""select BC.BORROWCARD_ID, LC.NAME, B.TITLE , BC.DUE_DATE, BC.RETURN_DATE
+                               from BORROWCARDS BC join LIBCARDS LC 
+                               on BC.LIBCARD_ID = LC.LIBCARD_ID 
+                               join BOOKS B
+                               on BC.BOOK_ID = B.BOOK_ID""")
+    result = cursor.fetchall()
+    return render(request, 'CardDetail.html', {'CardDetail':result})
+
+def CardEdit(request, id_bc):
+
+    result = cursor.execute(f"""select BC.BORROWCARD_ID, LC.NAME, B.TITLE , BC.DUE_DATE, BC.RETURN_DATE
+                               from BORROWCARDS BC join LIBCARDS LC 
+                               on BC.LIBCARD_ID = LC.LIBCARD_ID 
+                               join BOOKS B
+                               on BC.BOOK_ID = B.BOOK_ID
+                               WHERE BC.BORROWCARD_ID = {id_bc}""")
+    result = cursor.fetchall()
+    result[0][-2] = result[0][-2].strftime("%Y-%m-%d")
+    result[0][-1] = result[0][-1].strftime("%Y-%m-%d")
+    if result[0][-1] == "1900-01-01":
+        result[0][-1] = ""
+    return render(request, 'CardEdit.html', {'CardDetail':result})
+
+def CardUpdate(request):
+    card = library.models.CardDetail()
+    card.BORROWCARD_ID = request.POST.get('bc_id')
+    card.NAME = request.POST.get('name')
+    card.TITLE = request.POST.get('title')
+    card.DUE_DATE = request.POST.get('due_date')
+    card.RETURN_DATE = request.POST.get('return_date')
+    if card.RETURN_DATE=="1900-01-01":
+        cursor.execute(f"""UPDATE BORROWCARDS SET RETURN_DATE = NULL WHERE BORROWCARD_ID = {card.BORROWCARD_ID}""")
+    else:
+        cursor.execute(f"""UPDATE BORROWCARDS SET RETURN_DATE = '{card.RETURN_DATE}' WHERE BORROWCARD_ID = {card.BORROWCARD_ID}""")
+    cursor.commit()
+
+    return CardEdit(request,card.BORROWCARD_ID)
 
 def MemberAdd(request, *args, **kwargs):
     member = Libcards()
@@ -75,13 +120,12 @@ def MemberAdd(request, *args, **kwargs):
     member.address = request.POST.get('address')
     member.classroom = request.POST.get('class')
     if member.name != None and member.age != None and member.address != None and member.classroom != None:
-        cursor.execute(f"insert into LIBCARDS (NAME, AGES, ADDRESS, CLASS) +\
-                         values(N'{member.name}',{member.age},N'{member.address}',N'{member.classroom}')")
+        cursor.execute(f"""insert into LIBCARDS (NAME, AGES, ADDRESS, CLASS) 
+                         values(N'{member.name}',{member.age},N'{member.address}',N'{member.classroom}')""")
         cursor.commit()
         return render(request, "MemberAdd.html", {})
     else:
         return render(request, "MemberAdd.html", {})
 
-def Login(request, *args, **kwargs):
-        print(request.user)
-        return render(request, "Login.html", {})
+
+
